b39d8a9ca8e752786a4e066d52932b37054c9e8a
Gabriel Ebner <gebner@gebner.org>
2016-12-02 08:46:12 -0500

    Update Fri Dec  2 08:46:12 EST 2016

---
+++ js/nav_data.js
@@ -0,0 +1,1 @@
+var lean_nav_data=["01_Introduction.html","02_Dependent_Type_Theory.html","03_Propositions_and_Proofs.html","04_Quantifiers_and_Equality.html","05_Tactics.html","06_Inductive_Types.html","07_Induction_and_Recursion.html","08_Structures_and_Records.html","09_Type_Classes.html"];
